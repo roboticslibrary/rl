80c00e3f53f54f2cae3567941830b807ed97f130
zhaowenrui <zhaowenrui@tuodaomed.com>
2023-12-13 11:20:57 +0800

    fix iterator bugs in path optimizers

--- src/rl/plan/SimpleOptimizer.cpp
+++ src/rl/plan/SimpleOptimizer.cpp
@@ -51,7 +51,11 @@ namespace rl
 			{
 				changed = false;
 				
-				for (VectorList::iterator i = path.begin(), j = ::std::next(i), k = ::std::next(j); i != path.end() && j != path.end() && k != path.end(); ++i, ++j, ++k)
+				VectorList::iterator i = path.begin();
+				VectorList::iterator j = ++path.begin();
+				VectorList::iterator k = ++++path.begin();
+				
+				while (i != path.end() && j != path.end() && k != path.end())
 				{
 					::rl::math::Real ik = this->getModel()->distance(*i, *k);
 					
@@ -68,6 +72,12 @@ namespace rl
 						}
 						
 						changed = true;
+					}
+					else
+					{
+						++i;
+						++j;
+						++k;
 					}
 				}
 			}
